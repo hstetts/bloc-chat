338e10ff19304f7c7dde9b025bf6e0aaf2f7fe47
heidistetts <hstetts@gmail.com>
2017-11-12 11:50:10 -0500

    Checkpoint 3 Chatrooms:completed except for question on new room error

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,9 +1,22 @@
 (function() {
-    function HomeCtrl(Room, $scope) {
+    function HomeCtrl(Room, $uibModal, $scope) {
       this.rooms = Room.all;
+      this.currentRoom = null;
+
+      this.setRoom = function(room) {
+        this.currentRoom = room.getByRoomId(this.currentRoom.$id);
+      };
+
+      this.addRoom = function(){
+          $uibModal.open({
+          animation: this.animationsEnabled,
+          templateUrl: '/templates/new_room_modal.html',
+          controller: 'ModalCtrl as modal'
+      })
     };
+  }
 
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', '$scope', HomeCtrl]);
+        .controller('HomeCtrl', ['Room', '$uibModal', '$scope', HomeCtrl]);
 })();
---
+++ app/scripts/controllers/ModalCtrl.js
@@ -0,0 +1,18 @@
+(function(){
+  function ModalCtrl(Room, $uibModalInstance) {
+    var modal = this;
+
+    modal.submit = function() {
+      Room.add(modal.newRoom.name)
+      $uibModalInstance.close();
+    };
+
+    modal.cancel = function(){
+      $uibModalInstance.dismiss();
+    };
+}
+
+angular
+  .module('blocChat')
+  .controller('ModalCtrl', ['$uibModalInstance', 'Room', ModalCtrl]);
+})();
