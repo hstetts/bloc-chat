076e94e708d1dbde16f3dbcc72b8252fc97ef684
heidistetts <hstetts@gmail.com>
2017-11-14 07:31:45 -0500

    Revised Checkpoint 4 Messages

--- app/scripts/services/messages.js
+++ app/scripts/services/messages.js
@@ -4,12 +4,15 @@
     var ref = firebase.database().ref().child('messages');
     var messages = $firebaseArray(ref);
 
-    Message.getByRoomId = function(roomId) {
-      return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
-    };
+/* get roomId from firebase message object */
 
-    return Message;
-  }
+    Message.getByRoomId = function(roomId) {
+      //return $firebaseArray(ref.orderByChild('roomId').equalTo(roomId));
+
+    return messages;
+  };
+  return Message;
+};
 
   angular
     .module('blocChat')
